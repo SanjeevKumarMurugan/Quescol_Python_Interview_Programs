66a2dc2a3c1233eff188f52ea391aa961665555b
SanjeevKumar <sanjeevkumar.apj2@gmail.com>
2024-08-02 16:45:42 +0530

    method2 added

--- Removing_duplicates_from_dictionary.py
+++ Removing_duplicates_from_dictionary.py
@@ -40,3 +40,37 @@ for key, value in student_data.items():
 
 # Print the result
 print(unique_data)
+
+"""
+Method 2:
+student_data = {
+    'id1': {
+        'name': ['Sara'],
+        'class': ['V'],
+        'subject_integration': ['english, math, science']
+    },
+    'id2': {
+        'name': ['David'],
+        'class': ['V'],
+        'subject_integration': ['english, math, science']
+    },
+    'id3': {
+        'name': ['Sara'],
+        'class': ['V'],
+        'subject_integration': ['english, math, science']
+    },
+    'id4': {
+        'name': ['Surya'],
+        'class': ['V'],
+        'subject_integration': ['english, math, science']
+    }
+}
+
+result = {}
+
+for key, value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
+"""
